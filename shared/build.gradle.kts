1a6068bcac2365328b78f0a27720b3e9e1195a0d
Adam McNeilly <amcneilly331@gmail.com>
2023-07-04 14:24:44 -0400

    Formatting fixes.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("org.jmailen.kotlinter")
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
