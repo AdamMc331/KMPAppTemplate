b72764d3742bf29f6f1b13da64e92d004f418fba
Adam McNeilly <amcneilly331@gmail.com>
2023-07-04 14:59:26 -0400

    Adding shared module. (#7)
    Initial support for #2.

---
+++ shared/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jmailen.kotlinter")
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    android()
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "template.shared"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        @Suppress("MagicNumber")
+        jvmToolchain(17)
+    }
+}
